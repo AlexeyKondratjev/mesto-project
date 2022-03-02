0045751806d997884c057077625af467cddfecbf
Alexey Kondratjev <alexeykondratjev@yandex.ru>
2022-03-02 20:57:05 +0300

    Исправить ошибки после 1-го ревью.

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,13 @@ const elementAddForm = document.forms.elementAddForm;
 const imagePreviewPopup = document.querySelector('.popup_type_imagePreview');
 const imagePreviewPopupCloseButton = imagePreviewPopup.querySelector('.popup__toggle');
 
+const elementContainer = document.querySelector('.elements');
+
+const userName = profileEditForm.userName;
+const aboutYourself = profileEditForm.aboutYourself;
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -39,29 +46,27 @@ const initialCards = [
   }
 ];
 
-function openProfileEditPopup() {
-  const userName = profileEditForm.userName;
-  const aboutYourself = profileEditForm.aboutYourself;
-  const profileTitle = document.querySelector('.profile__title');
-  const profileSubtitle = document.querySelector('.profile__subtitle');
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
 
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function openProfileEditPopup() {
   userName.value = profileTitle.textContent;
   aboutYourself.value = profileSubtitle.textContent;
 
-  profileEditPopup.classList.add('popup_opened');
+  openPopup(profileEditPopup);
 }
 
 function closeProfileEditPopup() {
-  profileEditPopup.classList.remove('popup_opened');
+  closePopup(profileEditPopup);
 }
 
 function profileEditFormSubmitHandler(evt) {
   evt.preventDefault();
-
-  const userName = profileEditForm.userName;
-  const aboutYourself = profileEditForm.aboutYourself;
-  const profileTitle = document.querySelector('.profile__title');
-  const profileSubtitle = document.querySelector('.profile__subtitle');
 
   profileTitle.textContent = userName.value;
   profileSubtitle.textContent = aboutYourself.value;
@@ -70,22 +75,22 @@ function profileEditFormSubmitHandler(evt) {
 }
 
 function openElementAddPopup() {
-  elementAddPopup.classList.add('popup_opened');
+  openPopup(elementAddPopup);
 }
 
 function closeElementAddPopup() {
   elementAddForm.elementSrc.value = '';
   elementAddForm.elementName.value = '';
 
-  elementAddPopup.classList.remove('popup_opened');
+  closePopup(elementAddPopup);
 }
 
 function openImagePreviewPopup() {
-  imagePreviewPopup.classList.add('popup_opened');
+  openPopup(imagePreviewPopup);
 }
 
 function closeImagePreviewPopup() {
-  imagePreviewPopup.classList.remove('popup_opened');
+  closePopup(imagePreviewPopup);
 }
 
 function elementAddFormSubmitHandler(evt) {
@@ -93,9 +98,11 @@ function elementAddFormSubmitHandler(evt) {
 
   const imgSrcValue = elementAddForm.elementSrc.value;
   const titleValue = elementAddForm.elementName.value;
-  const newElementMarkup = getElementMarkup();
 
-  insertNewElement(newElementMarkup, imgSrcValue, titleValue);
+  const newElementMarkup = getElementMarkup();
+  const newElement = createCard(newElementMarkup, imgSrcValue, titleValue);
+
+  insertNewElement(newElement, elementContainer);
 
   closeElementAddPopup();
 }
@@ -106,23 +113,23 @@ function getElementMarkup() {
   return elementTemplate.querySelector('.element').cloneNode(true);
 }
 
-function insertNewElement(elementMarkup, imgSrcValue, titleValue) {
-  const elementContainer = document.querySelector('.elements');
-
+function createCard(elementMarkup, imgSrcValue, titleValue) {
   const elementImage = elementMarkup.querySelector('.element__image');
   elementImage.src = imgSrcValue;
+  elementImage.alt = titleValue;
 
   elementImage.addEventListener('click', function () {
     const imageToPreview = imagePreviewPopup.querySelector('.popup__image');
     const imageHeadingToPreview = imagePreviewPopup.querySelector('.popup__image-heading');
 
     imageToPreview.src = imgSrcValue;
+    imageToPreview.alt = titleValue;
     imageHeadingToPreview.textContent = titleValue;
 
     openImagePreviewPopup();
   });
 
-  elementMarkup.querySelector('.element__title').textContent = titleValue;
+   elementMarkup.querySelector('.element__title').textContent = titleValue;
 
   elementMarkup.querySelector('.element__like-button').addEventListener('click', function (evt) {
     evt.target.classList.toggle('like-button_active');
@@ -133,16 +140,13 @@ function insertNewElement(elementMarkup, imgSrcValue, titleValue) {
     deletedElement.remove();
   });
 
-  elementContainer.prepend(elementMarkup);
+  return elementMarkup;
 }
 
-function renderInitialElements() {
-  initialCards.forEach((item) => {
-    const newElementMarkup = getElementMarkup();
-
-    insertNewElement(newElementMarkup, item.link, item.name);
-  });
+function insertNewElement(card, container) {
+  container.prepend(card);
 }
+
 
 
 profileEditButton.addEventListener('click', openProfileEditPopup);
@@ -155,4 +159,11 @@ elementAddForm.addEventListener('submit', elementAddFormSubmitHandler);
 
 imagePreviewPopupCloseButton.addEventListener('click', closeImagePreviewPopup);
 
-renderInitialElements();
+
+
+initialCards.forEach((item) => {
+  const newElementMarkup = getElementMarkup();
+  const newElement = createCard(newElementMarkup, item.link, item.name);
+
+  insertNewElement(newElement, elementContainer);
+});
