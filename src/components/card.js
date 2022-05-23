77ec15741298f7145a06f92b10ac860625be367e
Alexey Kondratjev <alexeykondratjev@yandex.ru>
2022-05-23 18:32:11 +0300

    Classes Card, Validate, UserInfo changes (3)...

--- src/components/card.js
+++ src/components/card.js
@@ -1,106 +1,110 @@
-//Импорт данных из других модулей.
-import { imagePreviewPopup, configData } from '../utils/constants.js';
-import { openImagePreviewPopup, openDeleteConfirmPopup } from './modal.js';
-import Api from '../components/Api.js';
+export default class Card {
+  constructor({title, imageSrc, id, likesArray, cardOwnerId}, selector) {
+    this._title = title;
+    this._imageSrc = imageSrc;
+    this._id = id;
+    this._likesArray = likesArray;
+    this._cardOwnerId = cardOwnerId;
 
-let deletedCardId = '';
-const allFetches = new Api(configData);
-//Функция getElementMarkup получает шаблон элемента "карточка места" из HTML-разметки
+    this._selector = selector;
+  }
 
-//Функция getCardMarkup получает шаблон элемента "карточка места" из HTML-разметки
-//и возвращает клон соотв. узла DOM.
-function getCardMarkup() {
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cloneNode = cardTemplate.querySelector('.card').cloneNode(true);
+  //Метод _getElementMarkup получает шаблон элемента "карточки места" из HTML-разметки
+  //и возвращает клон соотв. узла DOM.
+  _getElementMarkup() {
+    return document.querySelector(this._selector).content.querySelector('.card').cloneNode(true);
+  }
 
-  return cloneNode;
-}
-
-//Функция setEventListeners принимает на вход параметр cardMarkup (HTML-разметка (шаблон) нового элемента "карточка места")
-//и задает обработчики событий интерактивным элементам, присутствующим на карточке.
-function setEventListeners(cardMarkup, imgSrcValue, titleValue) {
-  const cardImage = cardMarkup.querySelector('.card__image');
-  cardImage.src = imgSrcValue;
-  cardImage.alt = titleValue;
-
-  cardImage.addEventListener('click', function () {
-    const imageToPreview = imagePreviewPopup.querySelector('.popup__image');
-    const imageHeadingToPreview = imagePreviewPopup.querySelector('.popup__image-heading');
-
-    imageToPreview.src = imgSrcValue;
-    imageToPreview.alt = titleValue;
-    imageHeadingToPreview.textContent = titleValue;
-
-    openImagePreviewPopup();
-  });
-
-  cardMarkup.querySelector('.card__like-button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('like-button_active');
-
-    //Определяем метод запроса (что будем делать с лайками - добавлять, или удалять).
-    const queryMethod = evt.target.classList.contains('like-button_active') ? 'PUT' : 'DELETE';
-
-    //Изменяем информацию о лайках на сервере.
-    allFetches.changeLikesData(evt.target.closest('.element').id, queryMethod)
-
-      .then((result) => {
-        //Обновляем отображение значения счетчика лайков в карточке (на клиенте).
-        const likesCountElement = evt.target.closest('.card').querySelector('.card__likes-count');
-        likesCountElement.textContent = result.likes.length;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-
-  const deleteButton = cardMarkup.querySelector('.card__delete-button');
-
-  if (deleteButton) {
-    deleteButton.addEventListener('click', (evt) => {
-      deletedCardId = evt.target.closest('.card').id;
-      openDeleteConfirmPopup();
+  //Метод _isLikedByCurrentUser принимает на вход параметр currentUserId (уникальный идентификатор текущего пользователя).
+  //Возвращает true, если текущий пользователь есть среди пользователей, лайкнувших данную карточку, иначе - false.
+  _isLikedByCurrentUser(currentUserId) {
+    return this._likesArray.some((arrItem) => {
+      return arrItem._id === currentUserId;
     });
   }
-}
 
-//Функция likedByCurrentUser принимает на вход параметры currentUserId (уникальный идентификатор текущего пользователя)
-//и cardLikesArray (массив данных о пользователях, лайкнувших данную карточку).
-//Возвращает true, если текущий пользователь есть среди пользователей, лайкнувших карточку, иначе - false.
-function likedByCurrentUser(currentUserId, cardLikesArray) {
-  return cardLikesArray.some((arrItem) => {
-    return arrItem._id === currentUserId;
-  });
-}
-
-//Функция createCard принимает на вход параметры cardMarkup (HTML-разметка (шаблон) нового элемента "карточка места"),
-//imgSrcValue (URL-адрес "карточки места"), titleValue (название "карточки места"), idValue (уникальный идентификатор
-//"карточки места") и likesCountValue (количество лайеков).
-//Выполняет создание элемента новой "карточки места", заполняя шаблон данными, и устанавливая обработчики интерактивных событий.
-//Возвращает HTML-разметку готового элемента "карточки места".
-function createCard(cardMarkup, imgSrcValue, titleValue, idValue, likesArray, currentUserId, cardOwnerId) {
-  cardMarkup.querySelector('.card__title').textContent = titleValue;
-  cardMarkup.id = idValue;
-
-  //Задаем состояние кнопки лайка (лайкнул ли карточку текущий пользователь) и количество лайков в целом.
-  if (likedByCurrentUser(currentUserId, likesArray)) {
-    cardMarkup.querySelector('.card__like-button').classList.add('like-button_active');
-  } else {
-    cardMarkup.querySelector('.card__like-button').classList.remove('like-button_active');
-  };
-
-  cardMarkup.querySelector('.card__likes-count').textContent = likesArray.length;
-
-  //Определяем наличие кнопки удаления карточки (по условию: удалять можно только свои карточки).
-  if (currentUserId !== cardOwnerId) {
-    cardMarkup.querySelector('.card__delete-button').remove();
+  //Метод _currentUserIsOwner проверяет факт того, является ли текущий пользователь - владельцем данной карточки.
+  //Принимает на вход параметр currentUserId (уникальный идентификатор текущего пользователя).
+  //Возвращает true, если является, иначе - false.
+  _currentUserIsOwner(currentUserId) {
+    return this._cardOwnerId === currentUserId;
   }
 
-  //Устанавливаем обработчики событий для активных элементов карточки.
-  setEventListeners(cardMarkup, imgSrcValue, titleValue);
+  //Метод _setEventListeners задает обработчики событий интерактивным элементам, присутствующим на карточке.
+  _setEventListeners() {
+    const cardImage = this._element.querySelector('.card__image');
 
-  return cardMarkup;
+    cardImage.addEventListener('click', () => {
+      /* const imageToPreview = imagePreviewPopup.querySelector('.popup__image');
+      const imageHeadingToPreview = imagePreviewPopup.querySelector('.popup__image-heading');
+
+      imageToPreview.src = imgSrcValue;
+      imageToPreview.alt = titleValue;
+      imageHeadingToPreview.textContent = titleValue;
+
+      openImagePreviewPopup(); */
+    });
+
+    const cardLikeButton = this._element.querySelector('.card__like-button');
+
+    cardLikeButton.addEventListener('click', () => {
+      cardLikeButton.classList.toggle('like-button_active');
+
+      //Определяем метод запроса (что будем делать с лайками - добавлять, или удалять).
+      const queryMethod = cardLikeButton.classList.contains('like-button_active') ? 'PUT' : 'DELETE';
+
+      //Изменяем информацию о лайках на сервере.
+/*       changeLikesData(evt.target.closest('.card').id, queryMethod)
+        .then((result) => {
+          //Обновляем отображение значения счетчика лайков в карточке (на клиенте).
+          const likesCountElement = evt.target.closest('.card').querySelector('.card__likes-count');
+          likesCountElement.textContent = result.likes.length;
+        })
+        .catch((err) => {
+          console.log(err);
+        }); */
+    });
+
+    const cardDeleteButton = this._element.querySelector('.card__delete-button');
+
+    if (cardDeleteButton) {
+      cardDeleteButton.addEventListener('click', () => {
+        console.log('Removing card... id:', cardDeleteButton.closest('.card').id);
+        //deletedCardId = evt.target.closest('.card').id;
+        //openDeleteConfirmPopup();
+      });
+    }
+  }
+
+
+  //Метод generateCard выполняет создание элемента новой "карточки места", заполняя шаблон данными,
+  //и устанавливая обработчики интерактивных событий.
+  //Возвращает HTML-разметку готового элемента "карточки места".
+  generateCard(currentUserId) {
+    this._element = this._getElementMarkup();
+
+    this._element.querySelector('.card__title').textContent = this._title;
+    this._element.querySelector('.card__image').src = this._imageSrc;
+    this._element.querySelector('.card__image').alt = this._title;
+    this._element.id = this._id;
+
+    //Задаем состояние кнопки лайка (лайкнул ли карточку текущий пользователь) и количество лайков в целом.
+    if (this._isLikedByCurrentUser(currentUserId)) {
+      this._element.querySelector('.card__like-button').classList.add('like-button_active');
+    } else {
+      this._element.querySelector('.card__like-button').classList.remove('like-button_active');
+    };
+
+    this._element.querySelector('.card__likes-count').textContent = this._likesArray.length;
+
+    //Определяем наличие кнопки удаления карточки (по условию: удалять можно только свои карточки).
+    if (!this._currentUserIsOwner(currentUserId)) {
+      this._element.querySelector('.card__delete-button').remove();
+    };
+
+    //Устанавливаем обработчики событий для активных элементов карточки.
+    this._setEventListeners();
+
+    return this._element;
+  }
 }
-
-
-//Экспорт функций из модуля.
-export { deletedCardId, getCardMarkup, createCard };
