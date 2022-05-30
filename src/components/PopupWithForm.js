08c06f2761a6d3ae9b590afc75422ac03c2f862a
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2022-05-30 23:37:27 +0300

    Fix, made serch elemenst as constants, rename some func

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,34 +1,41 @@
 import Popup from "./Popup";
 
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, callbackFormSubmit, handleFormPrefill) {
+  constructor(popupSelector, handleFormSubmit, handleFormPrefill) {
     super(popupSelector);
-    this._formSubmit = callbackFormSubmit;
+    this._handleFormSubmit = handleFormSubmit;
     this._handleFormPrefill = handleFormPrefill;
+    this._inputList = this._popup.querySelectorAll('.form__item');
+    this._form = this._popup.querySelector('.popup__form');
+    this._formValues = {};
     // Кроме селектора попапа принимает в конструктор колбэк сабмита формы. В этом колбэке содержится метод класса Api.
   }
 
   _getInputValues() {
     //собирает данные всех полей формы.
-    const inputsList = this._popup.querySelectorAll('.form__item');
-    const inputValues = inputsList.map(input => input.value);
-    return inputValues;
+    this._inputList.forEach(input => this._formValues[input.name] = input.value);
+    console.log(this.formValues);
+    return this.formValues;
   }
 
   setEventListeners() {
     // Перезаписывает родительский метод setEventListeners. Метод setEventListeners класса PopupWithForm должен не только добавлять обработчик клика иконке закрытия, но и добавлять обработчик сабмита формы.
     super.setEventListeners();
-    this._popup.addEventListener('submit', this._formSubmit);
+    this._popup.addEventListener('submit', this._handleFormSubmit);
   }
 
   close() {
     // Перезаписывает родительский метод close, так как при закрытии попапа форма должна ещё и сбрасываться.
-    const form = this._popup.querySelector('.popup__form');
     super.close();
-    form.reset();
+    this._form.reset();
   }
 
-  formPrefill() {
+  submitForm() {
+    this._getInputValues();
+    this._handleFormSubmit();
+  }
+
+  prefillForm() {
     this._handleFormPrefill();
   }
 }
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,16 +3,15 @@ import Popup from "./Popup";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._imageToPreview = this._popup.querySelector('.popup__image');
+    this._imageHeadingToPreview = this._popup.querySelector('.popup__image-heading');
   }
 
   open(imgSrcValue, titleValue) {
     // Метод open перезаписывает родительский метод, вставляет в попап картинку с src изображения и подписью к картинке.
-    const imageToPreview = this._popup.querySelector('.popup__image');
-    const imageHeadingToPreview = this._popup.querySelector('.popup__image-heading');
-
-    imageToPreview.src = imgSrcValue;
-    imageToPreview.alt = titleValue;
-    imageHeadingToPreview.textContent = titleValue;
+    this._imageToPreview.src = imgSrcValue;
+    this._imageToPreview.alt = titleValue;
+    this._imageHeadingToPreview.textContent = titleValue;
     super.open();
   }
 }
